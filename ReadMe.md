9eae70383760820eb3498f225bd3e6d8625c3c49
Mr-emeka <speak2c.emeka@gmail.com>
2019-02-28 03:55:57 +0100

    final changes

---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+#MOVIE API(OMDb API)
+
+visit http://www.omdbapi.com/ to know more about OMDb API.
+
+
+
+
+
+
+#NOTE
+-run npm install
+-also create a keys.js file in your root directory.
+-Inpute your key and save
+ module.exports = {
+    OMBd_API_KEY: 'key'
+ }
--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-var app = require('express')();
-var request = require ('request');
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
 
+var keys = require('./keys');
+var apikey = keys.OMBd_API_KEY;
+
+
+var app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 
 
-app.get('/',function(req, res){
-    res.send("hello");
+app.get('/', function (req, res) {
+    // console.log(apikey);
+    res.render("home");
+
+})
+
+app.get('/result', function (req, res) {
+    var query = req.query.search;
+
+    var url = 'http://www.omdbapi.com/?s=' + query + '&apikey=' + apikey;
+
+    if (!query) {
+        res.redirect('/')
+    } else {
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var data = JSON.parse(body);
+                res.render('results', {
+                    data: data
+                })
+            }
+        })
+    }
+
+
+
 })
 
 
-app.listen(3000,function(){
+app.listen(3000, function () {
 
     console.log('Server started on port 3000')
 });
